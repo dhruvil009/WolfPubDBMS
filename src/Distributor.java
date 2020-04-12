eebfd72ae70141f71a1b0d7e41a5d593d1c81857
mtrawic <mtrawic@ncsu.edu>
2020-04-12 11:20:43 -0400

    Update Distributor is working!

--- src/Distributor.java
+++ src/Distributor.java
@@ -26,6 +26,7 @@ public class Distributor{
             System.out.println("1: Add a distributor");
             System.out.println("2: Show all distributors");
             System.out.println("3: Delete distributor");
+            System.out.println("4: Update a distributor");
             System.out.println("\n\n Enter your choice.");
 
             choice = s.nextInt();
@@ -56,8 +57,22 @@ public class Distributor{
                         }
                     }
                 }
-                case 3: try {                               // Show all the distributors
+                case 3: try {                               // Delete a distributor
                     deleteDistributor();
+                    break;
+                }catch (SQLException e){
+                    e.printStackTrace();
+                    if (connection != null) {
+                        try {
+                            connection.rollback();
+                        } catch(SQLException excep) {
+                            excep.printStackTrace();
+                        }
+                    }
+                }
+                case 4: try {                               // Update a distributor
+                    updateDistributor();
+                    break;
                 }catch (SQLException e){
                     e.printStackTrace();
                     if (connection != null) {
@@ -141,7 +156,7 @@ public class Distributor{
           String location = result.getString(6);
           double balance = result.getDouble(7);
           String city = result.getString(8);
-            System.out.println(
+          System.out.println(
                                account_no +
                                "\t" + type +
                                "\t" + name +
@@ -194,15 +209,142 @@ public class Distributor{
       }
     }
 
+    /*
+    * Prompts the user to delete a distributor by giving the account number.
+    */
     public static void deleteDistributor() throws SQLException {
         System.out.println("Enter Account_no of Distributor to delete:");
         int account_no = in.nextInt();
         in.nextLine();
 
         PreparedStatement pstmt = null;
-        pstmt = connection.prepareStatement("DELETE FROM Distributor WHERE Account_no = " + account_no + ";");
+        pstmt = connection.prepareStatement("DELETE FROM Distributor WHERE Account_no = ? ;");
+        pstmt.setInt(1, account_no);
         pstmt.executeUpdate();
 
+
+    }
+
+    /*
+    * Guides the user through updating the values in a row.
+    */
+    public static void updateDistributor() throws SQLException {
+        System.out.println("Enter Account_no of Distributor to update:");
+        int account_no = in.nextInt();
+        in.nextLine();
+        // Show the user the values of given accout number
+        PreparedStatement showDistributorStmt = null;
+        showDistributorStmt = connection.prepareStatement("SELECT * FROM Distributor WHERE Account_no = ?;");
+        showDistributorStmt.setInt(1, account_no);
+        result = showDistributorStmt.executeQuery();
+        //if (!result.next())
+        //  System.out.println("Distributor is empty.");
+        String type = "Dummy";
+        String name = "Dummy";
+        String phone_no = "Dummy";
+        String contact_person = "Dummy";
+        String location = "Dummy";
+        double balance = 0;
+        String city = "Dummy";
+        PreparedStatement update = null;
+        boolean foundAccount = false;
+        while (result.next()) {
+            foundAccount = true;
+            System.out.println();
+            account_no = result.getInt(1);
+            type = result.getString(2);
+            name = result.getString(3);
+            phone_no = result.getString(4);
+            contact_person = result.getString(5);
+            location = result.getString(6);
+            balance = result.getDouble(7);
+            city = result.getString(8);
+        }
+        if (!foundAccount) {
+          System.out.println("Couldn't find account: " + account_no);
+          return;
+        }
+        int choice;
+        do {
+            System.out.println("\n\n---------------- Update Menu ------------------");
+            // Account_no, type, name, phone_no, contact_person, location, balance, city
+            System.out.println( "Acnt_no \t type \t name \t phone_no \t contact \t location \t balance \t city");
+            System.out.println( account_no + "\t\t" + type + "\t" + name +  "\t\t" + phone_no +  "\t\t" + contact_person +
+                                 "\t\t" + location +  "\t\t" + balance +  "\t" + city);
+            System.out.println("1: Update type");
+            System.out.println("2: Update name");
+            System.out.println("3: Update phone number");
+            System.out.println("4: Update contact person");
+            System.out.println("5: Update location");
+            System.out.println("6: Update balance");
+            System.out.println("7: Update city");
+            System.out.println("8: Exit");
+            System.out.println("\n\n Enter your choice.");
+            /*
+            System.out.println("Enter city: ");
+            String city = in.nextLine();
+            */
+            update = connection.prepareStatement
+    ("UPDATE Distributor SET type = ?, name = ?, phone_no = ?, contact_person = ?, location = ?, balance = ?, city = ? WHERE Account_no = ?");
+            update.setString(1, type);
+            update.setString(2, name);
+            update.setString(3, phone_no);
+            update.setString(4, contact_person);
+            update.setString(5, type);
+            update.setDouble(6, balance);
+            update.setString(7, type);
+            update.setInt(8, account_no);
+            choice = in.nextInt();
+            in.nextLine();
+            switch(choice) {
+                case 1:
+                  System.out.println("Enter type: ('W' | 'B' | 'L')");
+                  type = "" + in.nextLine().charAt(0);
+                  update.setString(1, type);
+                  update.executeUpdate();
+                  break;
+                case 2:
+                  System.out.println("Enter name of distributor: ");
+                  name = in.nextLine();
+                  update.setString(2, name);
+                  update.executeUpdate();
+                  break;
+                case 3:
+                  System.out.println("Enter phone_no: i.e. 555-555-5555");
+                  phone_no = in.nextLine();
+                  update.setString(3, phone_no);
+                  update.executeUpdate();
+                  break;
+                case 4:
+                  System.out.println("Enter contact_person name: ");
+                  contact_person = in.nextLine();
+                  update.setString(4, contact_person);
+                  update.executeUpdate();
+                  break;
+                case 5:
+                  System.out.println("Enter location: ");
+                  location = in.nextLine();
+                  update.setString(5, type);
+                  update.executeUpdate();
+                  break;
+                case 6:
+                  System.out.println("Enter balance: ");
+                  balance = in.nextDouble();
+                  in.nextLine();
+                  update.setDouble(6, balance);
+                  update.executeUpdate();
+                  break;
+                case 7:
+                  System.out.println("Enter city: ");
+                  city = in.nextLine();
+                  update.setString(7, type);
+                  update.executeUpdate();
+                  break;
+                case 8:
+                  choice = 8;
+                  break;
+              }
+            } while (choice != 8);
 
     }
 
